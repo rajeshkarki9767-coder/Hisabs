f603ce17f145e30044101b14674357dea727abe5
rajeshkarki9767-coder <rajeshkarki9767@gmail.com>
2026-05-14 08:54:24 +0545

    commit

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 // changes meaningfully so old caches get purged on activate.
 // ============================================================
 
-const CACHE_VERSION = 'hisabs-v16';
+const CACHE_VERSION = 'hisabs-v17';
 const APP_SHELL = [
   './',
   './index.html',
